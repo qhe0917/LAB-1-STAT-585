e4a206ac1bfadde7a0f58e7add152713f50eb8af
xiyuansun2010 <xiyuansu@iastate.edu>
2019-02-06 17:17:40 -0600

    updated Kelly's test script

--- test_script_sxy.R
+++ test_script_sxy.R
@@ -1,30 +1,7 @@
 #command read_fwf from package readr
 library(readr)
 
-# 2. STATION LIST AND DATA FORMATS
-# 
-# FORMAT OF "ushcn-v2.5-stations.txt"
-# 
-# ----------------------------------------
-#   Variable             Columns    Type
-# ----------------------------------------
-#   COUNTRY CODE             1-2    Character
-# NETWORK CODE               3    Character
-# ID PLACEHOLDERS ("00")   4-5    Character
-# COOP ID                 6-11    Character
-# LATITUDE               13-20    Real
-# LONGITUDE              22-30    Real
-# ELEVATION              33-37    Real
-# STATE                  39-40    Character
-# NAME                   42-71    Character
-# COMPONENT 1 (COOP ID)  73-78    Character
-# COMPONENT 2 (COOP ID)  80-85    Character
-# COMPONENT 3 (COOP ID)  87-92    Character
-# UTC OFFSET             94-95    Integer
-# -----------------------------------------
-
-
-ushcn_data <- read_fwf("ushcn-v2.5-stations.txt",
+ushcn_data <- read_fwf("ftp://ftp.ncdc.noaa.gov/pub/data/ushcn/v2.5/ushcn-v2.5-stations.txt",
                        fwf_widths(c(2,1,2,7,
                                     9,11,6,
                                     3,31,7,7,7,
@@ -32,14 +9,22 @@ ushcn_data <- read_fwf("ushcn-v2.5-stations.txt",
                        c("country","network","ID","coop_ID","lat","long","elev","state","name","comp1","comp2","comp3","utc")),
                        col_types = cols('c','c','c','c','n','n','n','c','c','c','c','c','i'))
 head(ushcn_data)
+str(ushcn_data)
 
 #show lat, long, evaluation, time zone, state
 library(ggplot2)
 library(ggmap)
-
+library(maps)
+state=map_data("state")
+ushcn_data$utc=as.factor(ushcn_data$utc)
+#rename the utc levels
+levels(ushcn_data$utc) <- c("EST","CST","MST","PST")
 #us_map <- qmplot(long, lat, data = ushcn_data, colour = I('red'), size = I(0.5), darken = .2)
-
-theme_set(theme_bw(16))
+ggplot(data=state,aes(x=long,y=lat)) + 
+  geom_polygon(aes(group=group),col="grey",alpha=0.4)+
+  geom_point(data=ushcn_data,aes(x=long,y=lat,col=elev,shape=utc),size=0.8) +  
+  theme(text = element_text(size=10)) 
+#theme_set(theme_bw(16))
 # min(ushcn_data$lat)
 # min(ushcn_data$long)
 us_map <- get_map(c(left=min(ushcn_data$long), bottom=min(ushcn_data$lat), 
@@ -55,4 +40,9 @@ USMap +
                  bins = 50, geom = "polygon",
                  data = ushcn_data) +
   scale_fill_gradient(low = "black", high = "red")
+
+#http://www.storybench.org/plot-state-state-data-map-u-s-r/
+#https://socviz.co/maps.html
+
+
 
